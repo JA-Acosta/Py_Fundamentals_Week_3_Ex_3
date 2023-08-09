20afa87d840bea3178b3d052361db7e927ab4c64
JAAR <joe.aaron.acosta@outlook.com>
2023-08-09 15:00:36 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+>>> JAAR
+>>> 8/9/2023
+>>> Practicing Fundamentals Program 15
+>>> Version 0
+'''
+
+'''
+>>> Takes a csv file containing the first name, last name, and debt of an individual and prints it to the console. The program will then calculate the average debt for the list of individuals and will then create a list of individuals that are above the average and bellow the average.
+'''
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
